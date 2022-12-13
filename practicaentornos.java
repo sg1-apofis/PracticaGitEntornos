63ad55dbf43237d79d374fcfaa19512b31ceb08d
sg1-apofis <oscarjimmar@gmail.com>
2022-12-13 12:06:30 +0100

    cambios desde eclipse

--- practicaentornos.java
+++ practicaentornos.java
@@ -2,6 +2,7 @@
 
 public class practicaentornos {
     public static void main(String[] args) {
-        System.out.println("Practica entornos push desde intellij");
+        System.out.println("Practica entornos cambiado  desde intellij");
+        System.out.println("cambio");
     }
 }
